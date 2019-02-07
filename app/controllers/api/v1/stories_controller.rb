ac8008b2bd437c9476661e79ba14c944e39bf472
Metahaem <tom.rowson@gmail.com>
2019-02-07 14:36:02 +0000

    migrated and added routes

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::StoriesController < ApplicationController
+    
+    def index
+        @stories = Story.all
+        render json: @stories
+      end
+    
+      def show
+        render json: @story
+      end
+    
+      def create
+        @story = Story.new(act_params)
+        if @story.valid? && @story.save
+          render json: @story
+        else
+          render json: {error: "Unable to create story."}, status: 400
+        end
+      end
+
+      def scrape_all
+        Bbc.scrape_all_categories
+        Independent.scrape_all_categories
+        Sky.scrape_all_categories
+        Huff_post.scrape_all_categories
+        Guardian.scrape_all_categories
+        render json: @stories
+    end
+    
+    private
+    
+      def story_params
+        params.permit(:title, :subtext, :image, :link, user_id, category_id)
+      end
+    
+      def find_act
+        @story = Story.find(params[:id])
+      end
+    
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :websites
+      resources :categories
+      resources :stories
+      resources :user_stories
+      post 'scrape', to: 'stories#scrape_all'
+    end
+  end
+
 end
